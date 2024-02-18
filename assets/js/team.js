2e0c155f44cf8f7f7f32d8d4e7b57da55fbb8ad6
HackerShohag <HackerShohag@outlook.com>
2024-02-18 14:10:25 +0600

    hide unused social links

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,152 +1,173 @@
 //Raw data
 const advisor = [
-    {
-        name: "Md Rabiul Islam Sarker",
-        designation: "Professor",
-        dept: "Mechanical",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/advisor/robiul_islam.png",
-    },{
-        name: "Md. Wahedul Islam",
-        designation: "Associate Professor",
-        dept: "Mechanical",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/advisor/wahedul.jpg",
-    },    
-    {
-        name: "Miftahul Mobin Chowdhury Shabdik",
-        designation: "Lecturer",
-        dept: "Mechanical",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/advisor/shabdik.jpg",
-    },
-    {
-        name: "Mim Mashrur Ahmed",
-        designation: "Lecturer",
-        dept: "Mechanical",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/advisor/masrur.jpg",
-    },
+  {
+    name: "Md Rabiul Islam Sarker",
+    designation: "Professor",
+    dept: "Mechanical",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/advisor/robiul_islam.png",
+  },
+  {
+    name: "Md. Wahedul Islam",
+    designation: "Associate Professor",
+    dept: "Mechanical",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/advisor/wahedul.jpg",
+  },
+  {
+    name: "Miftahul Mobin Chowdhury Shabdik",
+    designation: "Lecturer",
+    dept: "Mechanical",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/advisor/shabdik.jpg",
+  },
+  {
+    name: "Mim Mashrur Ahmed",
+    designation: "Lecturer",
+    dept: "Mechanical",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/advisor/masrur.jpg",
+  },
 ];
 
-
 const team = [
-    {
-        name: "Zubayer Rahman Anik",
-        designation: "Team Captain",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/team/team-23/Captain.png",
-    },
-    {
-        name: "Md Rafi Khan",
-        designation: "Team Manager",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/team/team-23/manager.png",
-    },
-    {
-        name: "Md Fahim Shahriar",
-        designation: "Legislative Team Lead",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/team/team-23/legislativeTeamLead.png",
-    },
-    {
-        name: "Md Masum Al Mahmud",
-        designation: "Business Team Lead",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/team/team-23/businessTeamLead.png",
-    },
-    {
-        name: "Tammem Hasan",
-        designation: "Technical Team Lead",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/team/team-23/technicalTeamLead.png",
-    },
-    {
-        name: "Raian Hossain",
-        designation: "Design Team Lead",
-        email: "",
-        facebook: "",
-        linkedin: "",
-        website: "",
-        img: "./assets/img/team/team-23/designTeamLead.png",
-    },
-]
+  {
+    name: "Zubayer Rahman Anik",
+    designation: "Team Captain",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/team/team-23/Captain.png",
+  },
+  {
+    name: "Md Rafi Khan",
+    designation: "Team Manager",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/team/team-23/manager.png",
+  },
+  {
+    name: "Md Fahim Shahriar",
+    designation: "Legislative Team Lead",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/team/team-23/legislativeTeamLead.png",
+  },
+  {
+    name: "Md Masum Al Mahmud",
+    designation: "Business Team Lead",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/team/team-23/businessTeamLead.png",
+  },
+  {
+    name: "Tammem Hasan",
+    designation: "Technical Team Lead",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/team/team-23/technicalTeamLead.png",
+  },
+  {
+    name: "Raian Hossain",
+    designation: "Design Team Lead",
+    email: "",
+    facebook: "",
+    linkedin: "",
+    website: "",
+    img: "./assets/img/team/team-23/designTeamLead.png",
+  },
+];
 
-//html code ready
-const advisorHTML = advisor.map((item) => {
-    return`
+const advisorHTML = advisor
+  .map((item) => {
+    return `
         <div class="col-md-6 col-lg-6 item wow fadeIn">
-            <img class="rounded-circle" src="${item.img}" height="150px" width="150px">
+            <img class="rounded-circle" src="${
+              item.img
+            }" height="150px" width="150px">
             <h3 class="name">${item.name}</h3>
             <p class="description">${item.designation}
                 <br>Dept. of ${item.dept} Engineering
                 <br>Rajshahi University of Engineering & Technology
             </p>
             <div class="social">
-                <a href="${item.email}">
-                    <i class="fa fa-envelope"></i>
-                </a>
-                <a href="${item.facebook}" target="_blank">
-                    <i class="fa fa-facebook-official"></i>
-                </a>
-                <a href="${item.linkedin}" target="_blank">
-                    <i class="fa fa-linkedin"></i>
-                </a>
+                ${
+                  item.email
+                    ? `<a href="mailto:${item.email}"><i class="fa fa-envelope"></i></a>`
+                    : ""
+                }
+                ${
+                  item.facebook
+                    ? `<a href="${item.facebook}" target="_blank"><i class="fa fa-facebook-official"></i></a>`
+                    : ""
+                }
+                ${
+                  item.linkedin
+                    ? `<a href="${item.linkedin}" target="_blank"><i class="fa fa-linkedin"></i></a>`
+                    : ""
+                }
             </div>
         </div>
     `;
-}).join("")
+  })
+  .join("");
 
-const teamHTML = team.map((item) => {
-    return`
-    <div class="col-md-6 col-lg-4 item wow fadeIn">
-        <img class="rounded-circle" src="${item.img}" alt="${item.designation}">
-        <h3 class="name">${item.name}</h3>
-        <p class="description">${item.designation}</p>
-        <div class="social">
-            <a href="${item.email}">
-              <i class="fa fa-envelope"></i>
-            </a>
-            <a href="${item.facebook}" target="_blank">
-              <i class="fa fa-facebook-official"></i>
-            </a>
-            <a href="${item.linkedin}" target="_blank">
-              <i class="fa fa-linkedin"></i>
-            </a>
-            <a href="${item.website}" target="_blank">
-              <i class="fa fa-website"></i>
-            </a>
+const teamHTML = team
+  .map((item) => {
+    return `
+        <div class="col-md-6 col-lg-4 item wow fadeIn">
+                <img class="rounded-circle" src="${item.img}" alt="${
+      item.designation
+    }">
+                <h3 class="name">${item.name}</h3>
+                <p class="description">${item.designation}</p>
+                <div class="social">
+                        ${
+                          item.email
+                            ? `<a href="mailto:${item.email}"><i class="fa fa-envelope"></i></a>`
+                            : ""
+                        }
+                        ${
+                          item.facebook
+                            ? `<a href="${item.facebook}" target="_blank"><i class="fa fa-facebook-official"></i></a>`
+                            : ""
+                        }
+                        ${
+                          item.linkedin
+                            ? `<a href="${item.linkedin}" target="_blank"><i class="fa fa-linkedin"></i></a>`
+                            : ""
+                        }
+                        ${
+                          item.website
+                            ? `<a href="${item.website}" target="_blank"><i class="fa fa-website"></i></a>`
+                            : ""
+                        }
+                </div>
         </div>
-    </div>
-    `;
-}).join("")
+        `;
+  })
+  .join("");
 
 //push to html
 document.getElementById("team_advisor").innerHTML = advisorHTML;
